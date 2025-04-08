6584a511c06d3a57fc512de4882dd401ec5aaf3c
Osmolovsky K. <kirillosm09@gmail.com>
2025-04-08 13:43:02 +0300

    feat: add event filters

--- src/github_user_activity/github_activity.py
+++ src/github_user_activity/github_activity.py
@@ -1,35 +1,40 @@
 #!/usr/bin/python
 # Script is getting github user's activity
-# features: 
+# features:
 
 from urllib.request import urlopen
 from urllib.error import HTTPError
-import argparse 
+import argparse
 import json
 from pprint import pprint
 from collections import defaultdict
+from typing import Dict
 
 from .event_handler import Handler
 
-def collect_info(raw_activity: list) -> list:
-    """collect to hash table by [event_type][which repo]: how namy times"""
+
+def collect_info(raw_activity: list, fil: str = "DefaultEvent") -> list:
+    """Collect to hash table by [event_type][which repo]: how namy times"""
     handler = Handler()
-    info = defaultdict(lambda: defaultdict(int))
+    info: Dict[str, Dict[str, int]] = defaultdict(lambda: defaultdict(int))
     for event in raw_activity:
         event_type = event.get("type")
         repo_name = event.get("repo", {}).get("name")
+        if fil != "DefaultEvent" and event_type != fil:
+            continue
         if event_type and repo_name:
             info[event_type][repo_name] += 1
-
 
     printable_activities = []
 
     for event, subdict in info.items():
-        sub_h = handler.handler_map[event]
+        sub_h = handler.handler_map.get(event)
         printable_activities.extend(sub_h(subdict))
     return printable_activities
 
+
 def fetch_activity(args) -> bool:
+    """Fetch activities by api and print it. Return False if there is an error, True otherwise"""
     if not args.username:
         print("Username required")
         return False
@@ -37,12 +42,18 @@ def fetch_activity(args) -> bool:
         with urlopen(f"https://api.github.com/users/{args.username}/events") as r:
             body = r.read()
             activity = json.loads(body)
-            del body # delete unused data
-            
-            printable_info = collect_info(activity) # collect all info, like count how many times pushed
+            del body  # delete unused data
+
+            # collect all info, like count how many times pushed
+            if args.filter:
+                printable_info = collect_info(activity, args.filter)
+            else:
+                printable_info = collect_info(activity)
 
             for i in printable_info:
                 print(f"-- {i}")
+
+        return True
 
     except HTTPError:
         print(f"Error: there is no user named '{args.username}'")
@@ -51,11 +62,15 @@ def fetch_activity(args) -> bool:
 
 def main():
     parser = argparse.ArgumentParser(
-            prog="github-activity",
-            description="CLI script to fetch recent activity of GitHub user"
-            )
+        prog="github-activity",
+        description="CLI script to fetch recent activity of GitHub user",
+    )
     parser.add_argument("username")
-    parser.add_argument("--filter", choices=["one", "two"], help="filter user's activity by event type") #TODO: proper choices
+    parser.add_argument(
+        "--filter",
+        choices=[event for event in Handler().handler_map.keys()],
+        help="filter user's activity by event type",
+    )
     parser.set_defaults(func=fetch_activity)
 
     args = parser.parse_args()
